e19d50e035e367d66b1b7f0fae4da99cc9d0c5ca
Vitor-devs <vitorcoelho2004@outlook.com>
2023-05-15 17:20:49 -0300

    iniciando projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AdicionarComponent } from './components/adicionar/adicionar.component';
+import { UpdateComponent } from './components/update/update.component';
+import { DeleteComponent } from './components/delete/delete.component';
+import { ListarComponent } from './components/listar/listar.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:InicioComponent},
+  {path:'adicionarPet', component:AdicionarComponent},
+  {path:'adicionarDetalhesPet', component: UpdateComponent},
+  {path:'removerPet', component:DeleteComponent},
+  {path:'listarPets', component:ListarComponent},
+];
 
 @NgModule({
+  declarations: [],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomeContentComponent } from './components/home-content/home-content.component';
+import { AdicionarComponent } from './components/adicionar/adicionar.component';
+import { ListarComponent } from './components/listar/listar.component';
+import { UpdateComponent } from './components/update/update.component';
+import { DeleteComponent } from './components/delete/delete.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeContentComponent,
+    AdicionarComponent,
+    ListarComponent,
+    UpdateComponent,
+    DeleteComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
